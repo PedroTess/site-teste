66d0f7dc6e6878bdef0076c6ca0479dbd1a6bfbd
PedroTess <pedrotessniari@gmail.com>
2024-11-27 18:05:39 -0300

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,17 @@
-let navBar = document.querySelector('#header');
+let navBar = document.querySelector('#header')
 
-// Alteração da cor do header ao rolar
-document.addEventListener("scroll", () => {
-    let scrollTop = window.scrollY;
+document.addEventListener("scroll", ()=>{
+    let scrollTop = window.scrollY
 
-    if (scrollTop > 0) {
-        navBar.classList.add('rolar');
-    } else {
-        navBar.classList.remove('rolar');
+    if(scrollTop > 0){
+        navBar.classList.add('rolar')
+    }else{
+        navBar.classList.remove('rolar')
     }
-});
+})
 
-// Suavizar a rolagem ao clicar nos links
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
+    anchor.addEventListener('click', function(e) {
         e.preventDefault();
 
         const targetId = this.getAttribute('href').substring(1);
@@ -26,16 +24,33 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
             top: offsetPosition,
             behavior: "smooth"
         });
-
-        // Fechar o menu ao clicar
-        nav.classList.remove("active");
     });
 });
 
-// Abrir/fechar o menu com o botão hambúrguer
-const hamburger = document.querySelector(".hamburger");
-const nav = document.querySelector(".nav");
+// Menu de navegação mobile
 
-hamburger.addEventListener("click", () => {
-    nav.classList.toggle("active");
+let btnMenuMob = document.querySelector ('#btn-menu-mob')
+let line1 = document.querySelector ('.line-menumob-1')
+let line2 = document.querySelector ('.line-menumob-2')
+let menuMobile = document.querySelector ('#menu-mobile') 
+let body = document.querySelector ('body')
+
+btnMenuMob.addEventListener("click",()=>{
+    line1.classList.toggle('ativo1')
+    line2.classList.toggle('ativo2')
+    menuMobile.classList.toggle('abrir')
+    body.classList.toggle('no-overflow')
+})
+
+// Fechar o menu mobile ao clicar em um link
+document.querySelectorAll('#menu-mobile a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', () => {
+        // Verifica se o menu está aberto e, se sim, fecha
+        if (menuMobile.classList.contains('abrir')) {
+            line1.classList.remove('ativo1');
+            line2.classList.remove('ativo2');
+            menuMobile.classList.remove('abrir');
+            body.classList.remove('no-overflow');
+        }
+    });
 });
